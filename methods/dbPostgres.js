2e394976484cc9c7667c07e3ec4fae5aab92ed1c
Henrique Carlos <henriquecarlos361893@gmail.com>
2024-05-18 18:03:10 -0300

    Melhoria nas rotas do servidor e criação da rota de verificação das credenciais

--- methods/dbPostgres.js
+++ methods/dbPostgres.js
@@ -1,32 +1,22 @@
-import { randomUUID } from "crypto";
-import { sql } from "./db";
+import { sql } from "./db.js"
 
-export class DbPostgres {
+export class DbPostgresMethods {
     async read(username, password) {
         let client;
 
-        client = await sql`SELECT * FROM clients WHERE username = ${username}, password = ${password}`;
+        client = await sql`SELECT * FROM clients WHERE username = ${username} AND password = ${password}`;
 
-        return client;
+        return client[0];
     }
 
     async create(user) {
-
         const { username, email, password, confirm } = user;
 
         await sql`INSERT INTO clients (username, email, password) VALUES (${username}, ${email}, ${password})`
     }
 
-    async updateUsername(username, id) {
-        await sql`UPDATE clients SET username = ${username} WHERE id = ${id}`;
-    }
-
-    async updateEmail(email, id) {
-        await sql`UPDATE clients SET email = ${email} WHERE id = ${id}`;
-    }
-    
-    async updatePass(password, id) {
-        await sql`UPDATE clients SET password = ${password} WHERE id = ${id}`;
+    async update(username, email, password, id) {
+        await sql`UPDATE clients set username = ${username}, email = ${email}, password = ${password} WHERE id = ${id}`;
     }
 
     async del(username) {
--- scripts/server.js
+++ scripts/server.js
@@ -1,10 +1,10 @@
 import express, { json } from "express";
 import cors from "cors";
-import { DbPostgres } from "../methods/dbPostgres";
+import { DbPostgresMethods } from "../methods/dbPostgres.js"
 
 const server = express();
 const port = 8000;
-const database = new DbPostgres();
+const database = new DbPostgresMethods();
 
 server.use(json());
 
@@ -13,25 +13,57 @@ server.use(cors({
 }));
 
 server.post("/user", async (req, res) => {
-    const { username, email, password, confirm } = req.body;
-    
-    await database.create({
-        username,
-        email,
-        password,
-        confirm
-    });
+    const { userNameRegistered: username, emailRegistered: email, passwordRegistered: password, confirmRegistered: confirm } = req.body;
 
-    return res.status(201).send();
+    const clientVerify = database.read(username, password);
+
+    try {
+        if (clientVerify) {
+            return res.status(409).json({
+                success: false,
+                message: "Credentials already registered"
+            });
+        } else {
+            await database.create({
+                username,
+                email,
+                password,
+                confirm
+            });
+        
+            return res.status(201).json({
+                success: true,
+                message: "User registered successfully"
+            });
+        }
+    } catch (error) {
+        console.log("Error during user registration: ", error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        });
+    }
+
 });
 
-server.get("/user", async (req) => {
-    const search = req.query.search;
+server.post("/user/verify-user-credentials", async (req, res) => {
+    const { usernameLogin: username, passwordLogin: password } = req.body;
 
-    const users = await database.list(search);
-
-    return users;
-});
+    try {
+        const user = await database.read(username, password);
+        if (user) {
+            res.status(200).json({
+                message: 1
+            });
+        } else {
+            res.status(401).json({
+                message: 0
+            });
+        }
+    } catch (error) {
+        res.status(500).send();
+    }
+})
 
 server.listen(port, () => {
     console.log(`Server started at: localhost:${port}`);
